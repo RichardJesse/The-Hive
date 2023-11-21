3a7c44defcfb56c3d38cbaf5bf0ae3c5ff4e5937
EthieuRin <148864832+EthieuRin@users.noreply.github.com>
2023-11-21 13:29:07 +0300

    Add files via upload

---
+++ home.php
@@ -0,0 +1,50 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>NAIRO BEE WEBSITE</title>
+	<link rel="stylesheet"  href="style.css">
+	<!--box icons-->
+	<link rel="stylesheet"
+  href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,600;1,400;1,500&family=Lobster&family=Montserrat:ital,wght@0,300;0,400;1,400;1,500&family=Quicksand&display=swap" rel="stylesheet">
+</head>
+<body>
+	<header>
+		<a href="#" class="logo"><i class='bx bxs-home'></i>HONEY</a>
+
+		<ul class="navilist">
+			<li><a href="signup.php">Signup</a></li>
+			<li><a href="login.php">Login</a></li>
+			<li><a href="index.php" class="active">Home</a></li>
+			<li><a href="#about">AboutUs</a></li>
+			<li><a href="#shop">Ourshop</a></li>
+			<li><a href="#review">OurCustomer</a></li>
+			<li><a href="#contact">ContactUs</a></li>
+			<li><a href="profile.php">Profile</a></li>
+			<li><a href="logout.php">LogOut</a></li>
+
+			
+		</ul>
+
+		<div class="navi-icons">
+			<a href=""><i class='bx bx-search'></i></a>
+			<a href=""><i class='bx bxs-cart'></i></a>
+			<div class="bx bx-menu" id="menu-icon"></div>
+		</div>
+	 
+	</header>
+	<!--home-->
+	<section class="home" id="home">
+		<div class="home-text">
+			<h1>Meet<span>Eat &</span><br>Enjoy The True<br> Taste</h1>
+
+			
+		</div>
+
+		</body>
+</html>
